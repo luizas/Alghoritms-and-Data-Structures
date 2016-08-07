1c7c13d5c819ccd35ac8407ca27b5fa60a83806d
luizas <luiza.salek@gmail.com>
2016-08-07 22:47:56 +0100

    Create BubbleSortStrings.java

---
+++ BubbleSortStrings.java
@@ -0,0 +1,36 @@
+//Sort array of Strings- Bubble Sort
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BubbleSortStrings {
+        
+
+    public  static void main(String[] args) 
+    {
+		String[]tab={"Ola","Luiza","Arek","Aabi","Natalia"};
+		
+		int outer,inner;
+		
+		
+		for(outer=tab.length-1; outer>0;outer--)
+		{
+			for(inner=0;inner<outer;inner++)
+			{ int pom=tab[inner+1].compareTo(tab[inner]);
+				if (pom<0)
+				{
+					swap(inner,inner+1,tab);
+				}
+			}
+		}
+		System.out.println(Arrays.toString(tab));
+		}  
+	public static void swap(int first, int second, String[]tab)   
+	{
+		String temp=tab[first];
+		tab[first]=tab[second];
+		tab[second]=temp;
+		
+	}  
+    
+}
